abdad83c32f28c8a5a921f662a3ee8da851f088a
Kona <kitami.kitano20@gmail.com>
2021-01-25 21:47:47 +0900

    step05までdone

--- app/src/main.go
+++ app/src/main.go
@@ -2,8 +2,22 @@ package main
 
 import (
 	"fmt"
+	"sort"
 )
 
 func main() {
-	fmt.Println("Kona")
+	var n, x int
+	fmt.Scan(&n, &x)
+
+	t := make([]int, n)
+	for i := 0; i < n; i++ {
+		fmt.Scan(&t[i])
+	}
+	// [170 165 150 160 175 180 155]
+	fmt.Println("===========")
+
+	sort.Ints(t)
+	for i := 0; i < x; i++ {
+		fmt.Println(t[i])
+	}
 }
